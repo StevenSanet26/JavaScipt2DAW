d70d477033fc9fc70eca7e72690a3241c1b56a20
StevenSanet26 <stevensanet26@gmail.com>
2021-11-02 12:45:42 +0100

    js

--- TEMA5/2ExpresionesRegulares/ExpresionesRegulares.js
+++ TEMA5/2ExpresionesRegulares/ExpresionesRegulares.js
@@ -1,96 +1,100 @@
-window.onload= iniciar;
+window.onload = iniciar;
 
 
-function iniciar (){
-document.getElementById("enviar").addEventListener("click",enviar);
+function iniciar() {
+    document.getElementById("enviar").addEventListener("click", enviar, false);
 
 }
 
-function enviar(e){
+function enviar(e) {
+    console.log("gol");
     esborrarError();
-    if(validarDni() && validarFecha() && validarTel() && validarMatricula() && validarEmail() && validarUrl() && confirm("Confirma si vols enviar el formulari")){
+    if (validarDni() && validarFecha() && validarTel() && validarMatricula() && validarEmail() && validarUrl() && confirm("Confirma si vols enviar el formulari")) {
         return true;
-    }else{
+    } else {
         e.preventDefault();
         return false;
     }
 }
-function validarDni(){
-    var element= document.getElementById("dni");
-    if(!element.checkValidity()){
-        if (element.validity.valueMissing) {
-            error2(element,"Deus d'introduir un DNI.");
-        }
-        if (element.validity.patternMismatch) {
-            error2(element, "El DNI ha de tindre el format de 9 digits i una lletra.");
-        }
+function validarDni() {
+    var patroDni = new RegExp(/^\d{8}[a-zA-Z]$/);
+    var element = document.getElementById("dni");
+
+    if (patroDni.test(element.value)) {
+        return true;
+    } else {
+        error2(element, "Error: campo dni incorrecto");
         return false;
     }
-    return true;
-
-    
 }
-function validarFecha(){
-    var element= document.getElementById("fecha");
-    if (!element.checkValidity()) {
-        if (element.validity.valueMissing) {
-            error2(element,"Deus d'introduir una fecha.");
-        }
-        if (element.validity.rangerOverflow) {
-            error2(element, "La data mínima ha de ser superior al 01/12/1900.");
-        }
-        if (element.validity.rangerUnderfloe) {
-            error2(element, "La data màxima ha de ser inferior al 31/12/2021.");
-        }
-        if (element.validity.patternMismatch) {
-            error2(element, "La fecha ha de tindre el format YYYY/mm/dd");
-        }
+
+function validarFecha() {
+    var patroFecha = new RegExp(/^\d{4}\/(0?[1-9]|1[012])\/(0?[1-9]|[12][0-9]|3[01])$/);
+    var element = document.getElementById("fecha");
+
+    if (patroFecha.test(element.value)) {
+        return true;
+    } else {
+        error2(element, "Error: campo fecha incorrecto");
+        return false;
     }
 
 }
-function validarTel(){
+function validarTel() {
+    var patroTel = new RegExp(/^\d{3}\s\d{3}\s\d{3}$/);
     var element = document.getElementById("telefono");
-    if(!element.checkValidity()){
-        if (element.validity.valueMissing) {
-            error2(element,"Deus d'introduir un numero de Telèfon.");
-        }
-        if (element.validity.patternMismatch) {
-            error2(element, "El telèfon ha de trindre el format 999 999 999.");
-        }
+
+    if (patroTel.test(element.value)) {
+        return true;
+    } else {
+        error2(element, "Error: campo telefono incorrecto");
         return false;
     }
-    return true;
-
 }
-function validarMatricula(){
+
+
+function validarMatricula() {
+    var patroMatricula = new RegExp(/^\d{4} [A-Z]{3}$/);
     var element = document.getElementById("matricula");
-    if (!element.checkValidity()) {
-        if (element.validity.valueMissing) {
-            error2(element, "Deus de inntoduir un número de Matrícula");
-        }
-        if (element.validity.patternMismatch) {
-            error2(element,"La matrícula ha de tindre el fomat 9999 ZZZ");
-        }
+
+    if (patroMatricula.test(element.value)) {
+        return true;
+    } else {
+        error2(element, "Error: campo matricula incorrecto");
+    }
+}
+function validarEmail() {
+    var patroEmail = new RegExp(/^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/);
+    var element = document.getElementById("email");
+    if (patroEmail.test(element.value)) {
+        return true;
+    } else {
+        error2(element, "Error: campo email incorrecto");
         return false;
     }
-    return true;
-}
-function validarEmail(){
-
-}
-function validarUrl(){
-
 }
 
-function error2(element, missatge){
-    document,getElementById("missatgeError").innerHTML = missatge;
-    element.className= "error";
+function validarUrl() {
+    var patroUrl = new RegExp(/^((ftp|http|https):\/\/)?www\.([A-z]+)\.([A-z]{2,})/);
+    var element = document.getElementById("url");
+
+    if (patroUrl.test(element.value)) {
+        return true;
+    } else {
+        error2(element, "Error: campo url incorrecto");
+        return false;
+    }
+}
+
+function error2(element, missatge) {
+    document.getElementById("missatgeError").innerHTML = missatge;
+    element.className = "error";
     element.focus();
 }
 
-function esborrarError(){
+function esborrarError() {
     var formulari = document.forms[0];
-    for(var i =0; i<formulari.elements.length;i++){
-        formulari.elements[i].className= "";
+    for (var i = 0; i < formulari.elements.length; i++) {
+        formulari.elements[i].className = "";
     }
 }
