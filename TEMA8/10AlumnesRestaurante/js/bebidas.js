155d9da4bc28e19d27010538fb7f55d7472b0667
StevenSanet26 <stevensanet39@gmail.com>
2022-01-20 13:49:11 +0100

    AlumnesReserva

--- TEMA8/10AlumnesRestaurante/js/bebidas.js
+++ TEMA8/10AlumnesRestaurante/js/bebidas.js
@@ -1,9 +1,10 @@
 window.onload = main;
 
 function main() {
+
     comprobarToken();
-    cargarMesas();
-    document.getElementById("newMesa").addEventListener("click", nuevaMesa, false);
+    cargarBebidas();
+    document.getElementById("newBebida").addEventListener("click", nuevabebida, false);
     document.getElementById("confirmar").addEventListener("click", confirmar, false);
 }
 
@@ -38,14 +39,14 @@ function mostrarUsuario(usuari) {
     document.getElementById("avatar").setAttribute("src", "https://userprofile.serverred.es/public/img/" + usuari.avatar);
 }
 
-function cargarMesas() {
+function cargarBebidas() {
 
     let token;
     if (JSON.parse(localStorage.getItem("Token")) != null) {
         token = JSON.parse(localStorage.getItem("Token"));
     }
 
-    fetch("https://restaurante.serverred.es/api/mesas", {
+    fetch("https://restaurante.serverred.es/api/bebidas", {
         method: "GET",
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
@@ -54,19 +55,19 @@ function cargarMesas() {
         }
     }).then(response => response.json())
         .then(data => {
-            //console.log(data.data.data);
-            mostrarMesas(data.data.data);
+            console.log(data.data.data);
+            mostrarBebidas(data.data.data);
         });
 }
 
-function mostrarMesas(mesas) {
+function mostrarBebidas(mesas) {
 
     let fila = document.getElementById("files");
     fila.replaceChildren("");
     mesas.forEach((element, index) => {
-        
+
         let tr = document.createElement("tr");
-        
+
         let td1 = document.createElement("td");
         let borrar = document.createElement("button");
         borrar.setAttribute("class", "btn btn-primary btn-lg my-3");
@@ -86,30 +87,27 @@ function mostrarMesas(mesas) {
         td2.appendChild(modificar);
 
         let td3 = document.createElement("td");
-        let contenido3 = document.createTextNode(element.numero);
+        let contenido3 = document.createTextNode(element.nombre);
         td3.appendChild(contenido3);
 
         let td4 = document.createElement("td");
-        let contenido4 = document.createTextNode(element.comensales);
+        let contenido4 = document.createTextNode(element.precio);
         td4.appendChild(contenido4);
 
-        let td5 = document.createElement("td");
-        let contenido5 = document.createTextNode(element.descripcion);
-        td5.appendChild(contenido5);
+
 
         tr.appendChild(td1);
         tr.appendChild(td2);
         tr.appendChild(td3);
         tr.appendChild(td4);
-        tr.appendChild(td5);
+
 
         fila.appendChild(tr);
     });
 
 }
 
-
-function nuevaMesa() {
+function nuevabebida() {
     let formulario = document.getElementById("formulario");
     formulario.setAttribute("class", "");
 }
@@ -119,18 +117,17 @@ function confirmar(e) {
 
     e.preventDefault();
 
-    if (validarNumero() && validarComensales()) {
-        
-        let numero = document.getElementById("numero").value;
-        let comensales = document.getElementById("comensales").value;
-        let descripcion = document.getElementById("descripcion").value;
+    if (validarNombre() && validarPrecio()) {
+
+        let nombre = document.getElementById("nombre").value;
+        let precio = document.getElementById("precio").value;
+
         let idMesa = document.getElementById("_id");
         //console.log(idMesa.value);
-        
-        let mesa = {
-            "numero": numero,
-            "comensales": comensales,
-            "descripcion": descripcion
+
+        let bebida = {
+            "nombre": nombre,
+            "precio": precio
         }
 
         let token;
@@ -138,43 +135,43 @@ function confirmar(e) {
             token = JSON.parse(localStorage.getItem("Token"));
         }
 
-        if(idMesa.value==""){
+        if (idMesa.value == "") {
             console.log("se crea");
-            fetch("https://restaurante.serverred.es/api/mesas", {
+            fetch("https://restaurante.serverred.es/api/bebidas", {
                 method: "POST",
                 headers: {
                     'Content-Type': 'application/json',
                     "auth-token": token
                 },
-                body: JSON.stringify(mesa)
+                body: JSON.stringify(bebida)
             }).then(response => response.json())
                 .then(data => {
                     console.log(data.ok);
                     if (data.ok == true) {
                         alert("La mesa a sido creada");
+                        cargarBebidas();
+                    }
+                });
+        } else {
+            console.log("se modifica");
+            fetch("https://restaurante.serverred.es/api/mesas/" + idMesa.value, {
+                method: "PUT",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    "auth-token": token
+                },
+                body: JSON.stringify(mesa)
+            }).then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.ok == true) {
+                        alert("La mesa a sido modificada");
                         cargarMesas();
                     }
                 });
-        }else{
-            console.log("se modifica");
-            fetch("https://restaurante.serverred.es/api/mesas/"+idMesa.value, {
-            method: "PUT",
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                "auth-token": token
-            },
-            body: JSON.stringify(mesa)
-        }).then(response => response.json())
-            .then(data => {
-                console.log(data);
-                if (data.ok == true) {
-                    alert("La mesa a sido modificada");
-                    cargarMesas();
-                }
-            });
         }
-        
+
         return true;
     } else {
         return false;
@@ -182,47 +179,41 @@ function confirmar(e) {
 }
 
 
-function validarNumero() {
 
-    let element = document.getElementById("numero");
+
+function validarNombre() {
+
+    let element = document.getElementById("nombre");
     esborrarError(element);
 
     if (!element.checkValidity()) {
         if (element.validity.valueMissing) {
-            error2(element, "Numero querido");
+            error2(element, "Nombre requerido");
+        }
+        if (element.validity.patternMismatch) {
+            error2(element, "Nombre entre 6 y 60 caracteres");
+        }
+
+        return false;
+    }
+    return true;
+}
+
+function validarPrecio() {
+    let element = document.getElementById("precio");
+    esborrarError(element);
+    if (!element.checkValidity()) {
+        if (element.validity.valueMissing) {
+            error2(element, "Precio requerido");
         }
         if (element.validity.rangeUnderflow) {
-            error2(element, "Mínimo numero 1");
-        }
-        if (element.validity.rangeOverflow) {
-            error2(element, "Máximo numero 100");
+            error2(element, "Precio minimo 0€");
         }
         return false;
     }
     return true;
 }
 
-function validarComensales() {
-    let element = document.getElementById("comensales");
-    esborrarError(element);
-    if (!element.checkValidity()) {
-        if (element.validity.valueMissing) {
-            error2(element, "Nº Comensales requeridos");
-        }
-        if (element.validity.rangeUnderflow) {
-            error2(element, "Mínimo comensales 1");
-        }
-        if (element.validity.rangeOverflow) {
-            error2(element, "Máximo comensales 50");
-        }
-        return false;
-    }
-    return true;
-}
-
-function validarDescripcion() {
-    let element = document.getElementById("descripcion");
-}
 
 function error2(element, missatge) {
     document.getElementById("missatgeError").innerHTML = missatge;
@@ -236,69 +227,4 @@ function esborrarError(element) {
     /*for (var i = 0; i < formulari.elements.length; i++) {
         formulari.elements[i].className = "form-control";
     }*/
-}
-
-function borrarMesa(element) {
-    let id = element.id;
-    let token;
-    if (JSON.parse(localStorage.getItem("Token")) != null) {
-        token = JSON.parse(localStorage.getItem("Token"));
-    }
-
-    fetch("https://restaurante.serverred.es/api/mesas/" + id, {
-        method: "DELETE",
-        headers: {
-            "auth-token": token
-        }
-    }).then(response => response.json())
-        .then(data => {
-            console.log(data);
-            if (data.ok == true) {
-            console.log(data);
-            alert("S'ha borrat correctament la taule");
-            cargarMesas();
-            }
-        });
-}
-
-function modificarMesa(element) {
-    
-    formulario.setAttribute("class", "");
-    let token;
-
-    if (JSON.parse(localStorage.getItem("Token")) != null) {
-        token = JSON.parse(localStorage.getItem("Token"));
-    }
-
-    fetch("https://restaurante.serverred.es/api/mesas", {
-        method: "GET",
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Accept': 'application/json',
-            "auth-token": token
-        }
-    }).then(response => response.json())
-        .then(data => {
-            //console.log(data.data);
-            let id = element.id;
-            mostrarMesa(data.data.data, id);
-        });
-
-}
-function mostrarMesa(mesa, id) {
-
-    mesa.forEach(element => {
-        if (element._id == id) {
-            console.log(id);
-            let numero = document.getElementById("numero");
-            let idMesa = document.getElementById("_id");
-
-            let comensales = document.getElementById("comensales");
-            let descripcion = document.getElementById("descripcion");
-            numero.setAttribute("value", element.numero);
-            comensales.setAttribute("value", element.comensales);
-            descripcion.setAttribute("value", element.descripcion);
-            idMesa.setAttribute("value", id)
-        }
-    });
 }
